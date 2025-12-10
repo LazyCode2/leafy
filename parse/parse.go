d466d2f0ad9803e97541f598d3c03b34951bbbf3
Lazy_Developer <163510211+LazyCode2@users.noreply.github.com>
2025-12-10 16:28:48 +0600

    feat: add GetIndexTemplate function for index page
    Add a new function to load and parse the index HTML template for the index page.

--- parse/parse.go
+++ parse/parse.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Reads a markdown file from the given path and returns its raw bytes and filename.
-func GetContent(path string)([]byte , string) {
+func GetContent(path string) ([]byte, string) {
 	content, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatalf("cannot read markdown: %v", err)
@@ -16,7 +16,7 @@ func GetContent(path string)([]byte , string) {
 
 	filename := filepath.Base(path)
 
-	return content , filename
+	return content, filename
 }
 
 // Loads and parses the main HTML template used for page generation.
@@ -29,3 +29,12 @@ func GetTemplate(path string) *template.Template {
 	return tpl
 }
 
+// Load and Parse the index HTML template for index page
+func GetIndexTemplate(path string) *template.Template {
+	tpl, err := template.ParseFiles("template/index.tmpl")
+	if err != nil {
+		log.Fatalf("cannot load template: %v", err)
+	}
+
+	return tpl
+}
